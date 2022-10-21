dd52b39ad31ad68c1906f9f23284cc2b5427d649
Ratu Raihan Amany <ratu.amany@shopee.com>
2022-10-21 12:44:12 +0700

    Add tracker update data

--- pages/tracker.js
+++ pages/tracker.js
@@ -1,18 +1,55 @@
-const add = () => {
+const removeFormatting = (displayString) => {
+    cleanString = displayString.replace('.', '')
+    intValue = parseInt(cleanString)
+    return intValue
+}
+
+const addButtonClick = () => {
     let parent = document.getElementById("history")
+    let historyList = document.createElement("li")
     let inputDetail = document.getElementById("input-details")
     let inputAmount = document.getElementById("input-amount")
+    updatePrice()
 
-    if (inputAmount.value === 0){
+    if (inputAmount.value === "" && inputDetail.value === ""){
         return
     }
 
-    let historyDetails = document.getElementById("history-details")
-    let historyAmount = document.getElementById("history-amount")
-    document.getElementById("history-details").innerHTML = inputDetail.value;
-    historyAmount.innerHTML = inputAmount.value;
+    historyList.setAttribute("class", "list-group-item d-flex justify-content-between")
+    historyList.setAttribute("style", "border-right: 3px solid black")
+    historyList.innerHTML = 
+    `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-square-fill hide" viewBox="0 0 16 16">
+    <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm3.354 4.646L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 1 1 .708-.708z"/>
+    </svg>`+ inputDetail.value + `<span class="mx-2">` + inputAmount.value + `</span>`
 
-    parent.appendChild(historyDetails);
-    inputAmount.value = 0;
+    parent.appendChild(historyList);
+    inputAmount.value = "";
     inputDetail.value = "";
 }
+
+const updatePrice = () => {
+    let inputAmount = document.getElementById("input-amount")
+    let balanceAmount = document.getElementById("price-balance")
+    let expenseAmount = document.getElementById("price-expense")
+    let incomeAmount = document.getElementById("price-income")
+
+    // Input Formatting (string to integer)
+    let expense = removeFormatting(expenseAmount.innerHTML)
+    let income = removeFormatting(incomeAmount.innerHTML)
+    let input = removeFormatting(inputAmount.value)
+
+    if (input < 0) {
+        expense -= input
+        expenseAmount.innerHTML = expense
+    } else if (input > 0) {
+        income += input
+        incomeAmount.innerHTML = income
+    }
+
+    balanceAmount.innerHTML = (income - expense)
+}
+
+// document.getElementById("history").addEventListener("onclick", function(e){
+//     e.target.
+// })
+
